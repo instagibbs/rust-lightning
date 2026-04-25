0ab83c4abac3ed08bedf2be054e4c81c2e475d88
Leo Nash <hello@leonash.net>
2026-04-25 17:16:49 +0000

    proto taproot

--- lightning/src/ln/splicing_tests.rs
+++ lightning/src/ln/splicing_tests.rs
@@ -25,7 +25,7 @@ use crate::ln::channel::{
 use crate::ln::channelmanager::{provided_init_features, PaymentId, BREAKDOWN_TIMEOUT};
 use crate::ln::functional_test_utils::*;
 use crate::ln::funding::{FundingContribution, FundingContributionError, FundingTemplate};
-use crate::ln::msgs::{self, BaseMessageHandler, ChannelMessageHandler, MessageSendEvent};
+use crate::ln::msgs::{self, BaseMessageHandler, ChannelMessageHandler, MessageSendEvent, PartialSignatureWithNonce};
 use crate::ln::outbound_payment::RecipientOnionFields;
 use crate::ln::types::ChannelId;
 use crate::routing::router::{PaymentParameters, RouteParameters};
@@ -41,7 +41,6 @@ use crate::util::wallet_utils::{
 use crate::sync::Arc;
 
 use bitcoin::hashes::Hash;
-use bitcoin::secp256k1::ecdsa::Signature;
 use bitcoin::secp256k1::{PublicKey, Secp256k1, SecretKey};
 use bitcoin::transaction::Version;
 use bitcoin::SignedAmount;
@@ -49,6 +48,8 @@ use bitcoin::{
 	Amount, FeeRate, OutPoint as BitcoinOutPoint, Psbt, ScriptBuf, Transaction, TxOut, Txid,
 	WPubkeyHash, WScriptHash,
 };
+
+use musig_secp::musig::{PartialSignature, PublicNonce};
 
 #[test]
 fn test_splicing_not_supported_api_error() {
@@ -2362,7 +2363,7 @@ fn do_test_splice_reestablish(reload: bool, async_monitor_update: bool) {
 		create_announced_chan_between_nodes_with_value(&nodes, 0, 1, initial_channel_value_sat, 0);
 
 	let prev_funding_outpoint = get_monitor!(nodes[0], channel_id).get_funding_txo();
-	let prev_funding_script = get_monitor!(nodes[0], channel_id).get_funding_script();
+	let prev_funding_script = get_monitor!(nodes[0], channel_id).get_funding_script(&Secp256k1::new());
 
 	// Keep a pending HTLC throughout the reestablish flow to make sure we can handle them.
 	route_payment(&nodes[0], &[&nodes[1]], 1_000_000);
@@ -2768,7 +2769,7 @@ fn test_splice_confirms_on_both_sides_while_disconnected() {
 		create_announced_chan_between_nodes_with_value(&nodes, 0, 1, initial_channel_value_sat, 0);
 
 	let prev_funding_outpoint = get_monitor!(nodes[0], channel_id).get_funding_txo();
-	let prev_funding_script = get_monitor!(nodes[0], channel_id).get_funding_script();
+	let prev_funding_script = get_monitor!(nodes[0], channel_id).get_funding_script(&Secp256k1::new());
 
 	// Capture the pre-splice scid so we can later assert the announcement_sigs each side emits
 	// on reconnect carry the post-splice scid, not the pre-splice one the bug would emit.
@@ -2882,7 +2883,7 @@ fn test_holding_cell_claim_freed_after_inferred_splice_locked() {
 	let (_, _, channel_id, _) =
 		create_announced_chan_between_nodes_with_value(&nodes, 0, 1, initial_channel_value_sat, 0);
 	let prev_funding_outpoint = get_monitor!(nodes[0], channel_id).get_funding_txo();
-	let prev_funding_script = get_monitor!(nodes[0], channel_id).get_funding_script();
+	let prev_funding_script = get_monitor!(nodes[0], channel_id).get_funding_script(&Secp256k1::new());
 	let prev_scid = nodes[0].node.list_channels()[0].short_channel_id;
 
 	let (payment_preimage, payment_hash, ..) = route_payment(&nodes[0], &[&nodes[1]], 1_000_000);
@@ -3122,7 +3123,7 @@ fn do_test_propose_splice_while_disconnected(use_0conf: bool) {
 		.chain_monitor
 		.chain_monitor
 		.get_monitor(channel_id)
-		.map(|monitor| (monitor.get_funding_txo(), monitor.get_funding_script()))
+		.map(|monitor| (monitor.get_funding_txo(), monitor.get_funding_script(&Secp256k1::new())))
 		.unwrap();
 
 	// Negotiate the splice to completion. Node 1's quiescent action should be consumed by
@@ -4529,8 +4530,10 @@ fn test_splice_buffer_invalid_commitment_signed_closes_channel() {
 	let original_sig = acceptor_commit_sig.commitment_signed[0].signature;
 	let mut sig_bytes = original_sig.serialize_compact();
 	sig_bytes[0] ^= 0x01; // Flip a bit to corrupt the signature
+	let partial_signature = PartialSignature::from_byte_array((&sig_bytes[..32]).try_into().unwrap()).unwrap();
+	let public_nonce = PublicNonce::from_byte_array((&sig_bytes[32..]).try_into().unwrap()).unwrap();
 	acceptor_commit_sig.commitment_signed[0].signature =
-		Signature::from_compact(&sig_bytes).unwrap();
+		PartialSignatureWithNonce { partial_signature, public_nonce };
 
 	// Deliver the acceptor's invalid commitment_signed to the initiator BEFORE the initiator has
 	// called funding_transaction_signed. The message should be buffered, not processed.
